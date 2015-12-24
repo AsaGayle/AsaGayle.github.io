acc4800c4980b5178349d5d3d56f1ab71b68235d
AsaGayle <azmatch.gayle@yahoo.com>
2015-12-24 02:15:06 -0500

    Initial commit

---
+++ js/content.js
@@ -0,0 +1,25 @@
+content={
+    "C++":["<p class='Inconsolata language-info'>\
+           This is C++\
+           </p>"],
+    
+    "python":["<p class='Inconsolata language-info'>\
+            This is Python\
+            </p>"],
+    
+    "JavaScript":["<p class='Inconsolata language-info'>\
+            This is JavaScript\
+            </p>"],
+    
+    "HTML":["<p class='Inconsolata language-info'>\
+            This is HTML\
+            </p>"],
+    
+    "CSS":["<p class='Inconsolata language-info'>\
+            This is CSS\
+            </p>"],
+    
+    mySQL:["<p class='Inconsolata language-info'>\
+            This is MySQL\
+            </p>"]
+};
---
+++ js/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    
+    "use strict";
+    // Small program to make images with the .zoom class 
+    // to remove .faded class and expand image using webkit
+    // transitions
+    
+    // 1. start function on .zoom class when document is loaded
+        $('.zoom').hover(function (x) {
+            // 2.1 function obtains element's id and adds it to a variable
+            var getEl = $(x.target);
+            var item = getEl.attr('id');
+            insert(item);
+            // 2.2. function adds transition class and removes faded
+            // thus making image larger and back to normal opacity
+            $(this).addClass('transition');
+            $(this).removeClass('faded');
+            
+        }, 
+            // 2.3. function returns image back to nomarl
+            function () {
+            $(this).removeClass('transition');
+            $(this).addClass('faded');
+    });
+});
+
+// Basic function to insert content from content.js into 
+// language-info-box element on page
+function insert (id){
+    $('.language-info-box').html(content[id][0]);
+}
