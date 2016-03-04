265f431b3e00ad7d335ce6125bd8757f2b57d507
Asa Gayle <azmatch.gayle@yahoo.com>
2016-03-04 11:11:37 -0500

    Added Elusive Icons and fixed formatting of page

--- js/content.js
+++ js/content.js
@@ -1,25 +1,25 @@
 content={
-    "C++":["<p class='Bowlby language-info'>\
+    "C++":["<p class='Oswald language-info'>\
            C++\
            </p>"],
     
-    "python":["<p class='Bowlby language-info'>\
+    "python":["<p class='Oswald language-info'>\
             Python\
             </p>"],
     
-    "JavaScript":["<p class='Bowlby language-info'>\
+    "JavaScript":["<p class='Oswald language-info'>\
             JavaScript\
             </p>"],
     
-    "HTML":["<p class='Bowlby language-info'>\
+    "HTML":["<p class='Oswald language-info'>\
             HTML5\
             </p>"],
     
-    "CSS":["<p class='Bowlby language-info'>\
+    "CSS":["<p class='Oswald language-info'>\
             CSS3\
             </p>"],
     
-    mySQL:["<p class='Bowlby language-info'>\
+    mySQL:["<p class='Oswald language-info'>\
             MySQL\
             </p>"]
 };
